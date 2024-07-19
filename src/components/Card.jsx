52c943c6cb454231c625045c561e38f0b6f0bdd8
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-19 12:07:05 -0500

    feat: Update Card component to use dynamic image source

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({data, loading}) {
             return (
                 <li className='card-list' key={product.id}>
                     <p className='card-title'>{product.title}</p>
-                    <img className='card-image' src={'https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-5__en-IN.jpg?v=1715760161&width=823'} alt={product.description} />
+                    <img className='card-image' src={product.images} alt={product.description} />
                     <p className='card-price'>${product.price}</p>
                     <button className='card-button'> Add to cart 🛒 </button>
                 </li>
