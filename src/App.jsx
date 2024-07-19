f6f12556bb29478cf0ce4cde1b8503ff8c3be677
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-19 13:13:32 -0500

    chore: Update App component to remove unused code and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import { Card } from './components/Card';
-import { useFetchProducts } from './hooks/useFetchProducts'
-import { useFilters } from './hooks/useFilters';
 
 function App() {
-  const { products, isLoading } = useFetchProducts('https://dummyjson.com/products');
-  const { filters, setFilters } = useFilters()
-  const [ filterProducts, setFilteredProducts ] = useState(products.products)
-
-  useEffect(() => {
-    handlerFilterProducts(products)
-  }, [ filters ])
-
-  const handlerChangeInput = (value) => {
-    setFilters(prevState => {
-      return {
-        ...prevState,
-        minPrice: value
-      }
-    })
-  }
-
-  const handlerSelectCategory = (value) => {
-    setFilters(prevState => {
-      return {
-        ...prevState,
-        category: value
-      }
-    })
-  }
-
-  const handlerFilterProducts = (products) => {
-    const filterProducts = products.products?.filter( product => {
-      return (filters.category === 'all' || product.category === filters.category) && product.price >= filters.minPrice
-    })
-    setFilteredProducts(filterProducts)
-  }
-
   return (
-    <main className='container'>
-      <Header filters={filters} handlerChangeInput={handlerChangeInput} handlerSelectCategory={handlerSelectCategory} />
-      <Card data={filterProducts} loading={isLoading}/>
-    </main>
-  )
+      <main className='container'>
+        <Header />
+        <Card />
+      </main>
+    )
 }
 
 export default App
