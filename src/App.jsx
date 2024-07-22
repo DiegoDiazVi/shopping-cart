bc912fd3ab00fd82ae868610ed2642fdb189d1c2
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-22 17:05:37 -0500

    feat: Add Cart component and update App component to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header.jsx';
 import { Card } from './components/Card/Card.jsx';
 import FiltersProvider from './context/FiltersContext';
+import Cart from './components/Cart/Cart.jsx';
 
 function App() {
   return (
       <main className='container'>
         <FiltersProvider>
           <Header />
+          <Cart/>
           <Card />
         </FiltersProvider>
       </main>
