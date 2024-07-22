d441f8db3c143d797133dca458d7a46075b3b62c
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-22 16:27:03 -0500

    feat: Update App component to use FiltersProvider for managing filters in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/Header';
 import { Card } from './components/Card';
+import FiltersProvider from './context/FiltersContext';
 
 function App() {
   return (
       <main className='container'>
-        <Header />
-        <Card />
+        <FiltersProvider>
+          <Header />
+          <Card />
+        </FiltersProvider>
       </main>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import FiltersProvider from './context/FiltersContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <FiltersProvider>
+  <>
     <App />
-  </FiltersProvider>,
+  </>,
 )
