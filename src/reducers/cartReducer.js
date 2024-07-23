425a465fea04e426becde883f750e0bf300f2d50
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-22 19:03:46 -0500

    refactor: Update cartReducer to handle adding and removing products from the shopping cart

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,12 +1,20 @@
 export function cartReducer(cart, action) {
     switch (action.type) {
         case 'ADD_CART':{
-                const newCart = [...cart, { quantity: 1}]
+            const productInCartIndex = cart.findIndex(item => item.id === action.product.id)
+            if (productInCartIndex >= 0) {
+                const newCart = structuredClone(cart);
+                newCart[productInCartIndex].quantity += 1
                 return newCart
             }
-        case 'DELETE_CART':{
-            const newCart = []
+            const newCart = [...cart, {...action.product, quantity: 1 }]
             return newCart
+        }
+        case 'DELETE_CART':{
+            return []
+        }
+        case 'REMOVE_PRODUCT_CART':{
+            return cart.filter(item => item.id !== action.id)
         }
         default:
             throw new Error('Accion no conocida')
